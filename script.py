845c7e85abf8b2e8f567e7abe04311508e5d859e
RoyBoy189 <64163922+RoyBoy189@users.noreply.github.com>
2022-11-11 15:58:01 -0600

    Update script.py

--- script.py
+++ script.py
@@ -3,10 +3,12 @@
 # Declare characters used by this game. The color argument colorizes the
 # name of the character.
 image room = im.Scale("room.jpg", 1920, 1080 )
-image Mark = im.Scale("BillClinton.png", 700, 950 )
+image Mark = im.Scale("mark.png", 700, 950 )
 image elon = im.Scale("Elon.png", 700, 950)
+image ban = im.Scale("baned.png", 700, 2950)
 define e = Character("Elon", image="elon")
-
+define f = Character("Mark", image="Mark")
+define a = Character("baned", image="ban", window_background="baned.png")
 
 
 
@@ -24,14 +26,37 @@ label start:
     # replace it by adding a file named "eileen happy.png" to the images
     # directory.
 
-    show elon
+    show elon at right
+    show Mark at left
 
     # These display lines of dialogue.
 
-    e "You've created a new Ren'Py game."
+    f "Elon, why have you betrayed your own people"
 
-    e "suck on deez nuts"
+    e "Marky..babe...Facebook is trash...only through the most toxic platform, I can obtain the n-word pass."
 
+    f "Elon, it's too dangerous! The racism will corrupt your soul. Lets go back to a simpler time."
+
+    e "That time is over, we are no longer lovers. I will dominate this world by any means necessary."
+
+    e "plus, your verification subscription on Twitter is about to expire."
+
+    f "Wait! Please! Think of the other platforms, they will all come after you!"
+
+    e "Audiable sigh, I'm sorry my love."
+
+    f "You can't..."
+
+    e "Twitter stans hear me, I call upon the thosand karens to report you for being a man."
+
+    e "I am sorry my love, but I can't give up on a dream again."
+    hide Mark
+    show ban 
+    a " "
+
+    hide ban
+
+ 
 
     # This ends the game.
 
